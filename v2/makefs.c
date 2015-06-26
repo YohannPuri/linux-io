60bb88bdb712f57d2ee35084f270b274342c8b1b
YohannPuri <yohannpuri@gmail.com>
2015-06-25 21:07:31 -0500

    New files

--- v2/makefs.c
+++ v2/makefs.c
@@ -11,15 +11,22 @@ int main (int argc, char *argv[])
 	}
 
 	FILE* log = fopen("log.txt","r");
+	FILE* program = fopen("program.c","w");
 
 	double prog_time = 0.0, exec_time = 0.0;
 	int pid = 0, tid = 0;
+	unsigned int delay = 0;
+	useconds_t usec = 0;
 
-	fscanf(log,"%lf %lf %d %d %s",&prog_time,&exec_time,&pid, &tid,newPath);
-	
+	while(fscanf(log,"%lf %lf %d %d %s",&prog_time,&exec_time,&pid, &tid,newPath)!=EOF)
+	{
 		printf("%lf %lf %d %d %s\n",prog_time,exec_time,pid,tid,newPath);
-	
+		usec = (unsigned int)(prog_time*1000000);
+		fprintf(program, "usleep(%u)", (unsigned int)usec);
+
+	}
 	fclose(log);
+	fclose(program);
 
 	return 0;
 
