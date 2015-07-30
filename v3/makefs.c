c8e258862a451c38de2169fce804fc0a7f16d281
Yohann Puri <yohannpuri@gmail.com>
2015-07-30 16:22:12 -0500

    Changes

--- v3/makefs.c
+++ v3/makefs.c
@@ -531,7 +531,7 @@ int main (int argc, char *argv[])
 	int k = 0;
 	while(k<file_count)
 	{
-		fprintf(fileinfo,"%s\t\t%d\t%d\t%d\t%d\t%d\t%p\t%d\t%d\n",fs[k].filename,fs[k].pid,fs[k].tid,fs[k].bytes_read,fs[k].bytes_written,fs[k].fd,fs[k].stream_pointer,fs[k].fdi,fs[k].fpi);
+		fprintf(fileinfo,"%s\t\t%d\t%d\t\t%d\t\t%d\t%d\t%p\t%d\t%d\n",fs[k].filename,fs[k].pid,fs[k].tid,fs[k].bytes_read,fs[k].bytes_written,fs[k].fd,fs[k].stream_pointer,fs[k].fdi,fs[k].fpi);
 		k++;
 	}
 
